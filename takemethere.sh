35cbd34d134038c0cc7bb905e0ef9adb1005e622
Kamiel de Visser <kamieldevisser@gmail.com>
2024-06-25 20:32:51 +0200

    convert echos to EOF

--- takemethere.sh
+++ takemethere.sh
@@ -25,63 +25,66 @@ list_entries() {
 }
 
 print_options() {
-    echo "Options:"
-    echo "  n                   Change to the directory at line n (1-indexed)."
-    echo "  alias               Change to the directory associated with 'alias'."
-    echo "  -h, --help          Display an extended help message."
-    echo "  -a, --add           Add a new path or alias:path entry to the file."
-    echo "                        -a <path>"
-    echo "                        -a <alias> <path>"
-    echo "  -c, --change        Change alias or directory path by alias or line."
-    echo "                        -c <alias|n> <new_alias> <new_path>"
-    echo "                        -c <alias|n> <new_path>"
-    echo "  -d, --delete        Delete an alias or line by alias or line number."
-    echo "                        -d <alias|n>"
-    echo "  -D, --delete-all    Delete all entries in the .takemethere file."
-    echo "                      Asks for confirmation first, unless forced."
-    echo "                        -D [-f]"
-    echo "  -e, --edit          Open the ~/.takemethere file with your \$EDITOR."
-    echo "                      (This is powerful for quick rearranging of entries!)"
-    echo "  -l, --list          Display the current contents of the .takemethere file."
-    echo "  --examples          Display examples of how to use the script."
+    cat <<EOF
+Options:
+  n                   Change to the directory at line n (1-indexed).
+  alias               Change to the directory associated with 'alias'.
+  -h, --help          Display an extended help message.
+  -a, --add           Add a new path or alias:path entry to the file.
+                        -a <path>
+                        -a <alias> <path>
+  -c, --change        Change alias or directory path by alias or line.
+                        -c <alias|n> <new_alias> <new_path>
+                        -c <alias|n> <new_path>
+  -d, --delete        Delete an alias or line by alias or line number.
+                        -d <alias|n>
+  -D, --delete-all    Delete all entries in the .takemethere file.
+                      Asks for confirmation first, unless forced.
+                        -D [-f]
+  -e, --edit          Open the ~/.takemethere file with your \$EDITOR.
+                      (This is powerful for quick rearranging of entries!)
+  -l, --list          Display the current contents of the .takemethere file.
+  --examples          Display examples of how to use the script.
+EOF
 }
 
 print_examples() {
-    echo "Examples:"
-    echo "  tmt 3                         # Change to directory on line 3"
-    echo "  tmt my_alias                  # Change to directory associated with 'my_alias'"
-    echo ""
-    echo "  tmt -a /some/dir              # Add path '/some/dir' as the last entry."
-    echo "  tmt -a foo /some/dir          # Add alias 'foo' for path '/some/dir' as the last entry"
-    echo ""
-    echo "  tmt -c 1 /some/dir            # Change line 1 so it points to path '/some/dir'"
-    echo "  tmt -c 1 foo /some/dir        # Change line 1 so alias 'foo' points to path '/some/dir'"
-    echo "  tmt -c foo /some/dir          # Change alias 'foo' so it points to path '/some/dir'"
-    echo "  tmt -c foo bar /some/dir      # Change alias 'foo' to 'bar' and point it to '/some/dir'"
-    echo ""
-    echo "  tmt -d 3                      # Delete entry on line 3"
-    echo "  tmt -d foo                    # Delete entry associated with alias 'foo'"
-    echo ""
-    echo "  tmt -e                        # Edit the .takemethere file"
-    echo "  tmt -l                        # List current entries in .takemethere"
+    cat <<EOF
+Examples:
+  tmt 3                         # Change to directory on line 3
+  tmt my_alias                  # Change to directory associated with 'my_alias'
+
+  tmt -a /some/dir              # Add path '/some/dir' as the last entry.
+  tmt -a foo /some/dir          # Add alias 'foo' for path '/some/dir' as the last entry
+
+  tmt -c 1 /some/dir            # Change line 1 so it points to path '/some/dir'
+  tmt -c 1 foo /some/dir        # Change line 1 so alias 'foo' points to path '/some/dir'
+  tmt -c foo /some/dir          # Change alias 'foo' so it points to path '/some/dir'
+  tmt -c foo bar /some/dir      # Change alias 'foo' to 'bar' and point it to '/some/dir'
+
+  tmt -d 3                      # Delete entry on line 3
+  tmt -d foo                    # Delete entry associated with alias 'foo'
+
+  tmt -e                        # Edit the .takemethere file
+  tmt -l                        # List current entries in .takemethere
+EOF
 }
 
 print_help() {
-    echo "Usage: tmt [OPTIONS] [ARGUMENTS]"
-    echo ""
-    echo "Aliases: tmt, tkme, takemethere"
-    echo ""
-    echo "Description: TakeMeThere simplifies navigation to frequently visited directories using user-defined aliases stored in a user defined file (default: ~/.takemethere). Quickly access directories by alias (tmt work) or sequential order (tmt 1, tmt 2, etc.). Easily manage, reorder, and edit aliases through the CLI or using your preferred text editor."
-    echo ""
-    print_options
-    echo ""
-    echo "Note:"
-    echo "  - Line numbers and aliases are 1-indexed."
-    echo "  - Use quotes around alias:path entries containing spaces."
-    return 1
+    cat <<EOF
+Usage: tmt [OPTIONS] [ARGUMENTS]
+
+Aliases: tmt, tkme, takemethere
+
+Description: TakeMeThere simplifies navigation to frequently visited directories using user-defined aliases stored in a user defined file (default: ~/.takemethere). Quickly access directories by alias (tmt work) or sequential order (tmt 1, tmt 2, etc.). Easily manage, reorder, and edit aliases through the CLI or using your preferred text editor.
+
+$(print_options)
+
+Note:
+  - Line numbers and aliases are 1-indexed.
+  - Use quotes around alias:path entries containing spaces.
+EOF
 }
-
-
 
 # Argument is an alias when it is not a number group
 is_digits() {
