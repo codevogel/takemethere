ad947b00b15937559343f124873bc9f6ba8d4d36
Kamiel de Visser <kamieldevisser@gmail.com>
2024-07-03 23:39:15 +0200

    add --no-fzf flag to pick

--- src/pick_command.sh
+++ src/pick_command.sh
@@ -1,8 +1,8 @@
 go_to_prompt="Go to n or alias: "
 entries=$(tmt list)
-if [ $(which fzf) ]; then
-    target=$(echo "$entries" | fzf | cut -d'|' -f1)
+if [[ ! ${args[--no-fzf]} && $(which fzf) ]]; then
+    target=$(echo "$entries" | fzf --prompt "$go_to_prompt" --height 5 --reverse | cut -d'|' -f1)
 else
-    cat | read -p "$go_to_prompt" target
+    tmt list && read -p "$go_to_prompt" target
 fi
-tmt go $target
+echo $target
