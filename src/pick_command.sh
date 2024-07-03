5a96a902aaad6243f60cea635114f6a61e484654
Kamiel de Visser <kamieldevisser@gmail.com>
2024-07-03 17:10:21 +0200

    add fzf fallback picker

--- src/pick_command.sh
+++ src/pick_command.sh
@@ -1,1 +1,8 @@
-tmt list | fzf
+go_to_prompt="Go to n or alias: "
+entries=$(tmt list)
+if [ $(which fzf) ]; then
+    target=$(echo "$entries" | fzf | cut -d'|' -f1)
+else
+    cat | read -p "$go_to_prompt" target
+fi
+tmt go $target
